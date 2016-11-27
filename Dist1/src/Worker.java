d61eda2338b4f89aa26808aeaf9ab527d5d3cfaf
Yinon Eliraz <yinon@decor-d.com>
2016-11-27 16:44:50 +0200

    json analasys done

--- Dist1/src/Worker.java
+++ Dist1/src/Worker.java
@@ -11,29 +11,33 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Worker {
 
-	public static void main(String[] args) throws ClientProtocolException, IOException {
-		// TODO Auto-generated method stub
+	final static String NEAR_EARTH_OBJECTS="near_earth_objects";
+	final static String NAME="name";
+	final static String CLOSE_APPROACH="close_approach_data";
+	final static String RELATIVE_VELOCITY="relative_velocity";
+	final static String KPS="kilometers_per_second";
+	final static String HAZARDOUS="is_potentially_hazardous_asteroid";
+	final static String DIAMETER="estimated_diameter";
+	final static String METERS="meters";
+	final static String MAX_DIAMETER_METERS="estimated_diameter_max";
+	
+	public static void main(String[] args) throws ClientProtocolException, IOException, JSONException {
+
 		String url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=9hZ8Yyim2EC1ZrEs2XlySosrCTLXaVpqtQwo1SHJ";
 
-		//Dates
-		LocalDate date=LocalDate.parse("2014-02-28");
-		date=date.plusDays(1);
-		System.out.println(date);
+		LocalDate startDate=LocalDate.parse("2015-09-07");
 		
 		HttpClient client = HttpClientBuilder.create().build();
 		HttpGet request = new HttpGet(url);
 
-		// add request header
 		request.addHeader("User-Agent", "Dist1");
 		HttpResponse response = client.execute(request);
-
-		InputStream isr=response.getEntity().getContent();
-		System.out.println("Response Code : "
-		                + response.getEntity().getContent());
 
 		BufferedReader rd = new BufferedReader(
 			new InputStreamReader(response.getEntity().getContent()));
@@ -42,12 +46,34 @@ public class Worker {
 		String line = "";
 		while ((line = rd.readLine()) != null) {
 			result.append(line);
-			//System.out.println(line);
 		}
+		
 		System.out.println(result);
-		JSONObject json=new JSONObject(result);
-		
-		
+		final JSONObject json=new JSONObject(result.toString());
+		JSONObject near= json.getJSONObject(NEAR_EARTH_OBJECTS);
+		while(near!=null){
+			JSONArray arr=near.getJSONArray(startDate.toString());
+			for(int i=0;i<arr.length();i++){
+				JSONObject temp=arr.getJSONObject(i);
+				System.out.println("Astroide " + temp.getString(NAME));
+				String isHazard=determineAstroide(temp);
+				System.out.println(" " + isHazard);
+			}
+			startDate=startDate.plusDays(1);
+			if(!near.has(startDate.toString()))
+				near=null;
+		}		
 	}
+	
+	private static String determineAstroide(JSONObject astroide) throws JSONException{
+		if(astroide.getJSONArray(CLOSE_APPROACH).getJSONObject(0).getJSONObject(RELATIVE_VELOCITY).getDouble(KPS)<10 &&
+				astroide.getBoolean(HAZARDOUS))
+			return "GREEN";
+		if(astroide.getJSONArray(CLOSE_APPROACH).getJSONObject(0).getJSONObject(RELATIVE_VELOCITY).getDouble(KPS)>10 &&
+				astroide.getBoolean(HAZARDOUS) && 
+				astroide.getJSONObject(DIAMETER).getJSONObject(METERS).getDouble(MAX_DIAMETER_METERS)>200)
+			return "YELLOW";
 
+		return "RED";
+	}
 }
